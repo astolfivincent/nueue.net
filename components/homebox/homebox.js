74b2f16b5586ffb3398da226e2e6146c09ad963c
Vincent Astolfi <astolfivincent@gmail.com>
2017-07-15 18:01:54 -0600

    Form Submissions

--- components/homebox/homebox.js
+++ components/homebox/homebox.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
   $(".homebox__form").submit(function( event ) {
     event.preventDefault();
     var url = "/form-submissions.php";
+    alert($(this).serialize());
     $.ajax({
       type: "POST",
       url: url,
--- form-submissions.php
+++ form-submissions.php
@@ -1,7 +1,7 @@
 <?php
-$name = $_GET['name'];
-$email = $_GET['email'];
-$message = $_GET['message'];
+$name = htmlspecialchars_decode($_GET['name']);
+$email = htmlspecialchars_decode($_GET['email']);
+$message = htmlspecialchars_decode($_GET['message']);
 $to = 'astolfivincent@gmail.com';
 $subject = 'Contact Form Submission from '.$name;
 $headers = 'From: vincent@nueue.net' . "\r\n" . 'Reply-To: '. $email . "\r\n" . 'X-Mailer: PHP/' . phpversion();
