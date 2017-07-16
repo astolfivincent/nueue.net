b9d4ef1b8ab255556ef17ceccc31d891edd72b52
Vincent Astolfi <astolfivincent@gmail.com>
2017-07-15 18:21:02 -0600

    Debugging

--- components/homebox/homebox.js
+++ components/homebox/homebox.js
@@ -4,11 +4,10 @@ $(document).ready(function() {
   $(".homebox__form").submit(function( event ) {
     event.preventDefault();
     var url = "/form-submissions.php";
-    alert($(this).serialize());
     $.ajax({
       type: "POST",
       url: url,
-      data: $(this).serialize(),
+      data: {name: 'name'},
       success: function(data) {
          console.log(data);
       }
