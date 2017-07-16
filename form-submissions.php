34d63011dd0d1064b982a3ecee9b2e64920bd4c5
Vincent Astolfi <astolfivincent@gmail.com>
2017-07-15 18:40:57 -0600

    Debugging

--- form-submissions.php
+++ form-submissions.php
@@ -2,17 +2,12 @@
 $name = htmlspecialchars_decode($_POST['name']);
 $email = htmlspecialchars_decode($_POST['email']);
 $message = htmlspecialchars_decode($_POST['message']);
-/*
-$email = htmlspecialchars_decode($_GET['email']);
-$message = htmlspecialchars_decode($_GET['message']);
 $to = 'astolfivincent@gmail.com';
 $subject = 'Contact Form Submission from '.$name;
 $headers = 'From: vincent@nueue.net' . "\r\n" . 'Reply-To: '. $email . "\r\n" . 'X-Mailer: PHP/' . phpversion();
 
 if ($name && $email && $message) {
   mail($to, $subject, $message, $headers);
+  echo 'Submission Accepted';
 }
-*/
-
-echo $email;
 ?>
