70e8957e5e8a363d9c68b33d358932310afecd14
Vincent Astolfi <astolfivincent@gmail.com>
2017-07-15 18:30:22 -0600

    Debugging

--- components/homebox/homebox.js
+++ components/homebox/homebox.js
@@ -2,12 +2,13 @@ $ = require('jQuery');
 
 $(document).ready(function() {
   $(".homebox__form").submit(function( event ) {
+    var name = $(this).child("input[name=name]");
     event.preventDefault();
     var url = "/form-submissions.php";
     $.ajax({
       type: "POST",
       url: url,
-      data: {name: 'name'},
+      data: {name: name},
       success: function(data) {
          console.log(data);
       }
