676eb52ba6710a3c824158de5d395a795263bac9
Vincent Astolfi <astolfivincent@gmail.com>
2017-07-16 19:07:53 -0600

    Error messaging for homepage form

--- components/homebox/homebox.js
+++ components/homebox/homebox.js
@@ -1,26 +1,44 @@
 $ = require('jQuery');
 
+function validateEmail(email) {
+  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
+
 $(document).ready(function() {
   $(".homebox__form").submit(function( event ) {
     event.preventDefault();
+    var $homeboxError = $('.homebox__form-error');
     var name = $(".homebox__form input[name=name]").val();
     var email = $(".homebox__form input[name=email]").val();
     var message = $(".homebox__form textarea[name=message]").val();
     var url = "/form-submissions.php";
-    $.ajax({
-      type: "POST",
-      url: url,
-      data: {
-        name: name,
-        email: email,
-        message: message
-      },
-      success: function(data) {
-         if (data == 'Submission Accepted') {
-           $(".homebox__form").hide();
-           $('.homebox__form-message').toggleClass('homebox__form-message-show');
-         }
+    if (validateEmail(email) && mail && name) {
+      $.ajax({
+        type: "POST",
+        url: url,
+        data: {
+          name: name,
+          email: email,
+          message: message
+        },
+        success: function(data) {
+           if (data == 'Submission Accepted') {
+             $(".homebox__form").hide();
+             $('.homebox__form-message').toggleClass('homebox__form-message-show');
+           }
+        }
+      });
+    } else {
+      if (!name) {
+        $homeboxError.show().append('<p>What\'s your name!?</p>');
       }
-    });
+      if (!validateEmail(email)) {
+        $homeboxError.show().append('<p>Please enter a valid email address</p>');
+      }
+      if (!message) {
+        $homeboxError.show().append('<p>Please enter a message</p>');
+      }
+    }
   });
 });
