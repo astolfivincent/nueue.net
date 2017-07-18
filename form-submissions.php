b39613500c99881162203e676df802b0a831ce19
Vincent Astolfi <astolfivincent@gmail.com>
2017-07-17 18:55:48 -0600

    Updating email headers

--- form-submissions.php
+++ form-submissions.php
@@ -4,7 +4,7 @@ $email = htmlspecialchars_decode($_POST['email']);
 $message = htmlspecialchars_decode($_POST['message']);
 $to = 'astolfivincent@gmail.com';
 $subject = 'Contact Form Submission from '.$name;
-$headers = 'From: vincent@nueue.net' . "\r\n" . 'Reply-To: '. $email . "\r\n" . 'X-Mailer: PHP/' . phpversion();
+$headers = 'From: Nueue Contact <vincent@nueue.net>' . "\r\n" . 'Reply-To: '. $name .'<'. $email . "> \r\n" . 'X-Mailer: PHP/' . phpversion();
 
 if (isset($name) && isset($email) && isset($message)) {
   mail($to, $subject, $message, $headers);
