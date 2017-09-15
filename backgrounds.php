b36d9c81e7da7c13f34d9173f29feb64e986eda2
Vincent Astolfi <astolfivincent@gmail.com>
2017-09-14 22:39:32 -0600

    Finalizing backgrounds.php updates for background generation

--- backgrounds.php
+++ backgrounds.php
@@ -1,24 +1,38 @@
 <?php
   header('Access-Control-Allow-Origin: *');
+  $images = glob('images/backgrounds' . '/*.*');
+  $rand_opts = array(
+    'rand_min' => 0,
+    'rand_max' => 999999999,
+  );
+  $gank_opts = array(
+    'url' => 'https://images.weserv.nl/?il&url=unsplash.it/960/480/?random&'.rand($rand_opts['rand_min'], $rand_opts['rand_max']),
+    'backgrounds' => $images
+  );
 
-  function random_image($dir) {
-    $files = glob($dir . '/*.*');
+  function random_image($files) {
     $file = array_rand($files);
     return $files[$file];
   }
 
-  $myObj = Array(random_image('images/backgrounds'));
-  $myJSON = json_encode($myObj);
-  $file = fopen('components/background/background.json','w+');
-  fwrite($file, $myJSON);
-  fclose($file);
-
-  function gank_image($g,$a,$n,$k) {
-    $r = rand($g, $a);
-    copy($n, $k.'image-'.$r.'.jpg');
-    return $k.'image-'.$r.'.jpg';
+  function set_next_background($url) {
+    $url = Array($url);
+    $json = json_encode($url);
+    $file = fopen('components/background/background.json','w+');
+    fwrite($file, $json);
+    fclose($file);
   }
 
-  gank_image(0, 999999999,'https://images.weserv.nl/?il&url=unsplash.it/960/480/?random', 'images/backgrounds/');
+  function gank_image($g,$a,$n,$k) {
+    if (count($k) < 100) {
+      $r = rand($g, $a);
+      $u = 'images/backgrounds/image-'.$r.'.jpg';
+      copy($n, $u);
+      return $u;
+    }
+  }
+
+  set_next_background(random_image($gank_opts['backgrounds']));
+  gank_image($rand_opts['rand_min'], $rand_opts['rand_max'], $gank_opts['url'], $gank_opts['backgrounds']);
 
 ?>
