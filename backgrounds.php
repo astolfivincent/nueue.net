795f0717f01b63139d666abd5f7ec1e213705db5
Vincent Astolfi <astolfivincent@gmail.com>
2017-09-16 16:18:13 -0600

    Reducing background acquisition complexity, no render blocking js

--- backgrounds.php
+++ backgrounds.php
@@ -4,41 +4,30 @@
   $request = $_POST['req'];
   $gank_opts = array(
     'filesize_max' => 70000, // Max filesize for image in bytes
+    'files_max' => 100,
     'url' => 'https://images.weserv.nl/?il&url=unsplash.it/960/480/?random&',
     'backgrounds' => glob('images/backgrounds' . '/*.*')
   );
 
   if (isset($request) && is_numeric($request) && $request === '0') {
-    function remote_file_size($url){
-    	$data = get_headers($url, true);
-    	if (isset($data['Content-Length']))
-    	return (int) $data['Content-Length'];
-    }
-
     function gank_image($g,$a,$n,$k) {
-      $m = 100;
       $c = count($k);
-      if (isset($a)) {
-        if($g >= $a) {
-          $n = $n.rand(1, 99999999);
-          if ($c < $m) {
-            $v = $c + 1;
-            $u = 'images/backgrounds/image-'. $v .'.jpg';
-            copy($n, $u);
-          } else {
-            echo 'Reached maximum number of images: '.$m;
-          }
-        } else {
-          return gank_image($g,$a,$n,$k);
-        }
+      $n = $n.rand(1, 99999999);
+      if ($c < $a) {
+        $v = $c + 1;
+        $u = 'images/backgrounds/image-'. $v .'.jpg';
+        copy($n, $u);
+      } else {
+        echo 'Reached maximum number of images: '.$a;
       }
     }
-
-    gank_image($gank_opts['filesize_max'], remote_file_size($gank_opts['url']), $gank_opts['url'], $gank_opts['backgrounds']);
+    gank_image($gank_opts['filesize_max'], $gank_opts['files_max'], $gank_opts['url'], $gank_opts['backgrounds']);
   } else if (isset($request) && is_numeric($request) && $request > 0 && $request <= 100) {
       $count = count($gank_opts['backgrounds']);
       $req_img = 'images/backgrounds/image-'.$request.'.jpg';
-      delete($req_img)
+      unlink($req_img);
       rename('images/backgrounds/image-'.$count.'.jpg', $req_img);
+  } else {
+    echo 'broken af';
   }
 ?>
