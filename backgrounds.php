769640b22e41a4ba05e0d62accfa7737535c9b71
Vincent Astolfi <astolfivincent@gmail.com>
2017-09-16 09:49:27 -0600

    Adding background curator, basic

--- backgrounds.php
+++ backgrounds.php
@@ -1,37 +1,44 @@
 <?php
   header('Access-Control-Allow-Origin: *');
 
-  function remote_file_size($url){
-  	$data = get_headers($url, true);
-  	if (isset($data['Content-Length']))
-  	return (int) $data['Content-Length'];
-  }
-
-  function gank_image($g,$a,$n,$k) {
-    $m = 100;
-    $c = count($k);
-    if (isset($a)){
-      if($g >= $a) {
-        $n = $n.rand(1, 99999999);
-        if ($c < $m) {
-          $v = $c + 1;
-          $u = 'images/backgrounds/image-'. $v .'.jpg';
-          copy($n, $u);
-        } else {
-          echo 'Reached maximum number of images: '.$m;
-        }
-      } else {
-        return gank_image($g,$a,$n,$k);
-      }
-    }
-  }
-
+  $request = $_POST['req'];
   $gank_opts = array(
     'filesize_max' => 70000, // Max filesize for image in bytes
     'url' => 'https://images.weserv.nl/?il&url=unsplash.it/960/480/?random&',
     'backgrounds' => glob('images/backgrounds' . '/*.*')
   );
 
-  gank_image($gank_opts['filesize_max'], remote_file_size($gank_opts['url']), $gank_opts['url'], $gank_opts['backgrounds']);
+  if (isset($request) && is_numeric($request) && $request === '0') {
+    function remote_file_size($url){
+    	$data = get_headers($url, true);
+    	if (isset($data['Content-Length']))
+    	return (int) $data['Content-Length'];
+    }
 
+    function gank_image($g,$a,$n,$k) {
+      $m = 100;
+      $c = count($k);
+      if (isset($a)) {
+        if($g >= $a) {
+          $n = $n.rand(1, 99999999);
+          if ($c < $m) {
+            $v = $c + 1;
+            $u = 'images/backgrounds/image-'. $v .'.jpg';
+            copy($n, $u);
+          } else {
+            echo 'Reached maximum number of images: '.$m;
+          }
+        } else {
+          return gank_image($g,$a,$n,$k);
+        }
+      }
+    }
+
+    gank_image($gank_opts['filesize_max'], remote_file_size($gank_opts['url']), $gank_opts['url'], $gank_opts['backgrounds']);
+  } else if (isset($request) && is_numeric($request) && $request > 0 && $request <= 100) {
+      $count = count($gank_opts['backgrounds']);
+      $req_img = 'images/backgrounds/image-'.$request.'.jpg';
+      delete($req_img)
+      rename('images/backgrounds/image-'.$count.'.jpg', $req_img);
+  }
 ?>
--- components/background/background.js
+++ components/background/background.js
@@ -1,24 +1,30 @@
 $ = require('jQuery');
 $(document).ready(function() {
-  function getRandomArbitrary(min, max) {
+  function backgroundRand(min, max) {
     return parseInt(Math.random() * (max - min)) + min;
   }
-  rand = Math.floor(getRandomArbitrary(1,100));
-  src = 'images/backgrounds/image-' + rand + '.jpg';
+  function backgroundAjax(req) {
+    $.ajax({
+      type: 'POST',
+      url: 'http://nueue.net/backgrounds.php',
+      data: {
+        req: req
+      },
+      success: function(data) {
+        console.log(data);
+      }
+    });
+  }
+  rand = Math.floor(backgroundRand(1,100));
+  src = 'http://nueue.net/images/backgrounds/image-' + rand + '.jpg';
   $background = $('.background__image');
   $background.load(function() {
     $(this).addClass('background__image-unhide');
+    backgroundAjax(0);
   });
   $background.attr('src', src);
 
-  $.ajax({
-    type: "POST",
-    url: 'http://nueue.net/backgrounds.php',
-    data: {
-      requesting: 1
-    },
-    success: function(data) {
-      console.log(data);
-    }
+  $('.background__curator').bind('click', function() {
+    backgroundAjax(rand);
   });
 });
