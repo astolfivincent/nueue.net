bd430dbf8e68d5f63859fab7f7c38cc1a0ede4e7
Vincent Astolfi <astolfivincent@gmail.com>
2017-10-15 01:01:23 +0100

    Better accessibility, improving submission UX

--- components/homebox/homebox.js
+++ components/homebox/homebox.js
@@ -6,17 +6,30 @@ function validateEmail(email) {
 }
 
 $(document).ready(function() {
-  $(".homebox__form").submit(function( event ) {
+  $(".homebox__form").submit(function(event) {
     event.preventDefault();
     var $homeboxError = $('.homebox__form-error');
-    var nameError = '<p>What\'s your name!?</p>'; // Name Error text
-    var emailError = '<p>Please enter a valid email address</p>'; // Email Error text
-    var messageError = '<p>Please enter a message</p>'; // Message Error text
+    var nameError = '<p aria-live="polite">What\'s your name!?</p>'; // Name Error text
+    var emailError = '<p aria-live="polite">Please enter a valid email address</p>'; // Email Error text
+    var messageError = '<p aria-live="polite">Please enter a message</p>'; // Message Error text
     var name = $(".homebox__form input[name=name]").val();
     var email = $(".homebox__form input[name=email]").val();
     var message = $(".homebox__form textarea[name=message]").val();
 
+    function homeboxLoader(b) {
+      $homeboxFormSubmit = $('.homebox__form input[name=submit]');
+      $homeboxFormLoader = $('.homebox__form-loader');
+      if (b === true) {
+        $homeboxFormSubmit.hide();
+        $homeboxFormLoader.show();
+      } else {
+        $homeboxFormSubmit.show();
+        $homeboxFormLoader.hide();
+      }
+    }
+
     if (validateEmail(email) && message && name) {
+      homeboxLoader(true);
       $.ajax({
         type: "POST",
         url: '/form-submissions.php',
@@ -30,6 +43,7 @@ $(document).ready(function() {
              $(".homebox__form").hide();
              $('.homebox__form-message').toggleClass('homebox__form-message-show');
            } else if (data == 'Invalid Email') {
+             homeboxLoader(false);
              $homeboxError.show().append(emailError);
            }
         }
