e1c84046582f5bc0ea613db3981b52162680af26
Vincent Astolfi <astolfivincent@gmail.com>
2017-10-15 17:01:56 +0100

    Removing test stuff

--- backgrounds.php
+++ backgrounds.php
@@ -1,33 +1,29 @@
 <?php
-  header('Access-Control-Allow-Origin: *');
+$request = $_POST['req'];
+$gank_opts = array(
+  'filesize_max' => 70000, // Max filesize for image in bytes
+  'files_max' => 100,
+  'url' => 'https://images.weserv.nl/?il&url=unsplash.it/960/480/?random&',
+  'backgrounds' => glob('images/backgrounds' . '/*.*')
+);
 
-  $request = $_POST['req'];
-  $gank_opts = array(
-    'filesize_max' => 70000, // Max filesize for image in bytes
-    'files_max' => 100,
-    'url' => 'https://images.weserv.nl/?il&url=unsplash.it/960/480/?random&',
-    'backgrounds' => glob('images/backgrounds' . '/*.*')
-  );
-
-  if (isset($request) && is_numeric($request) && $request === '0') {
-    function gank_image($g,$a,$n,$k) {
-      $c = count($k);
-      $n = $n.rand(1, 99999999);
-      if ($c < $a) {
-        $v = $c + 1;
-        $u = 'images/backgrounds/image-'. $v .'.jpg';
-        copy($n, $u);
-      } else {
-        echo 'Reached maximum number of images: '.$a;
-      }
+if (isset($request) && is_numeric($request) && $request === '0') {
+  function gank_image($g,$a,$n,$k) {
+    $c = count($k);
+    $n = $n.rand(1, 99999999);
+    if ($c < $a) {
+      $v = $c + 1;
+      $u = 'images/backgrounds/image-'. $v .'.jpg';
+      copy($n, $u);
+    } else {
+      echo 'Reached maximum number of images: '.$a;
     }
-    gank_image($gank_opts['filesize_max'], $gank_opts['files_max'], $gank_opts['url'], $gank_opts['backgrounds']);
-  } else if (isset($request) && is_numeric($request) && $request > 0 && $request <= 100) {
-      $count = count($gank_opts['backgrounds']);
-      $req_img = 'images/backgrounds/image-'.$request.'.jpg';
-      unlink($req_img);
-      rename('images/backgrounds/image-'.$count.'.jpg', $req_img);
-  } else {
-    echo 'broken af';
   }
+  gank_image($gank_opts['filesize_max'], $gank_opts['files_max'], $gank_opts['url'], $gank_opts['backgrounds']);
+} else if (isset($request) && is_numeric($request) && $request > 0 && $request <= 100) {
+    $count = count($gank_opts['backgrounds']);
+    $req_img = 'images/backgrounds/image-'.$request.'.jpg';
+    unlink($req_img);
+    rename('images/backgrounds/image-'.$count.'.jpg', $req_img);
+}
 ?>
