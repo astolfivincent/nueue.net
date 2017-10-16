907a0a212ae31e63415feb06bd24c96ebf96a114
Vincent Astolfi <astolfivincent@gmail.com>
2017-10-16 14:53:15 +0100

    Refactoring homebox.js to clear errors on every submit

--- components/homebox/homebox.js
+++ components/homebox/homebox.js
@@ -1,45 +1,46 @@
 $ = require('jQuery');
+var $homeboxError = $('.homebox__form-error');
+var nameError = '<p aria-live="polite">What\'s your name!?</p>'; // Name Error text
+var emailError = '<p aria-live="polite">Please enter a valid email address</p>'; // Email Error text
+var messageError = '<p aria-live="polite">Please enter a message</p>'; // Message Error text
+var timeError = '<p aria-live="polite">Too many submissions; wait 20 seconds, try again.</p>';
+var name = $(".homebox__form input[name=name]").val();
+var email = $(".homebox__form input[name=email]").val();
+var message = $(".homebox__form textarea[name=message]").val();
 
 function validateEmail(email) {
   var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(email);
 }
 
+function homeboxCleanErrors() {
+  $homeboxError.empty();
+}
+
+function homeboxError(error, clean) {
+  if (clean == true) {
+    homeboxCleanErrors();
+  }
+  $homeboxError.show().append(error);
+}
+
+function homeboxLoader(b) {
+  $homeboxFormSubmit = $('.homebox__form input[name=submit]');
+  $homeboxFormLoader = $('.homebox__form-loader');
+  if (b === true) {
+    $homeboxFormSubmit.hide();
+    $homeboxFormLoader.show();
+  } else {
+    $homeboxFormSubmit.show();
+    $homeboxFormLoader.hide();
+  }
+}
+
 $(document).ready(function() {
   $(".homebox__form").submit(function(event) {
     event.preventDefault();
-    var $homeboxError = $('.homebox__form-error');
-    var nameError = '<p aria-live="polite">What\'s your name!?</p>'; // Name Error text
-    var emailError = '<p aria-live="polite">Please enter a valid email address</p>'; // Email Error text
-    var messageError = '<p aria-live="polite">Please enter a message</p>'; // Message Error text
-    var timeError = '<p aria-live="polite">Too many submissions; wait 20 seconds, try again.</p>';
-    var name = $(".homebox__form input[name=name]").val();
-    var email = $(".homebox__form input[name=email]").val();
-    var message = $(".homebox__form textarea[name=message]").val();
-
-    function homeboxLoader(b) {
-      $homeboxFormSubmit = $('.homebox__form input[name=submit]');
-      $homeboxFormLoader = $('.homebox__form-loader');
-      if (b === true) {
-        $homeboxFormSubmit.hide();
-        $homeboxFormLoader.show();
-      } else {
-        $homeboxFormSubmit.show();
-        $homeboxFormLoader.hide();
-      }
-    }
-
-    function homeboxCleanErrors() {
-      $homeboxError.empty();
-    }
-
-    function homeboxError(error, clean) {
-      if (clean == true) {
-        homeboxCleanErrors();
-      }
-      $homeboxError.show().append(error);
-    }
-
+    homeboxCleanErrors();
+    
     if (validateEmail(email) && message && name) {
       homeboxLoader(true);
       $.ajax({
